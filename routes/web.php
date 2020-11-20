11f6811917eb2b5699e725fe14517a0de6bc1a1f
daichi1123 <badass.haruzion@gmail.com>
2020-11-20 10:06:50 +0000

    registration users

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// get←新規登録フォームを表示させる
+// post←ユーザ登録したい情報を送信する
+
+// Route::get('アドレス(〇〇/{パラメータ})', '関数orコントローラ名@アクション名')->name('ルーティングに名前をつける');
+// Viewファイルでアクション名は使われる・アクション名は自分で決めることが可能

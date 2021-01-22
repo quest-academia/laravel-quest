00a628c0a445775ea0d3bb6d11ba86433ccf8275
Nao <awakomik@gmail.com>
2021-01-22 02:41:31 +0000

    third commit

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class"mt-5">
+    
+    <nav class="navbar navbar-light bg-light justify-content-center">
+        <div class="novbar-brand m-0">©︎ QuestAcademia, All rights reserved.</div>
+    </nav>
+    
+</footer>

9a546a548cfdabece92837d7975d800dbeaf87c9
KyoheiYamamoto <57794595+KyoheiYamamoto@users.noreply.github.com>
2021-02-03 23:02:48 +0900

    Revert "Laravel Quest 4章完了(nao)"

--- app/User.php
+++ app/User.php
@@ -26,10 +26,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    
-    public function movies()
-    {
-        return $this->hasMany(Movie::class);
-    }
-    
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,7 @@
     <div class="text-center text-white">
         <h1>YouTubeまとめ × SNS</h1>
     </div>
-    
+        
 </div>
 
-<div class="text-right">
-    
-    @if(Auth::check())
-        {{ Auth::user()->name }}
-    @endif
-    
-</div>
-
-    @include('users.users', ['users'=>$users])
 @endsection

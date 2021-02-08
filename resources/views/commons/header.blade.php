9f92f9961e8fa5b8481d72bf52c56d3262ce454e
Otani <daguru0409@gmail.com>
2021-02-08 03:09:16 +0000

    laravel-quest４章終了

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -12,11 +12,12 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 
+            
                 @if (Auth::check())
                 
                     <li class="nav-item">{!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
-                    <li class="nav-item"><a href="" class="nav-link">マイページ</a></li>
-                    
+                    <li class="nav-item">{!! link_to_route('users.show','マイページ',['id'=>Auth::id()],['class'=>'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('movies.create','動画を登録する',['id'=>Auth::id()],['class'=>'nav-link']) !!}</li>
                 @else
                 
                     <li class="nav-item">{!! link_to_route('signup', '新規ユーザ登録', [], ['class' => 'nav-link']) !!}</li>

6cfe61750b7c4d9204b442cc0d29f429f13b2c3f
Otani <daguru0409@gmail.com>
2021-02-14 04:36:47 +0000

    Laravel-Quest7章完了

--- youtube-curation8888/resources/views/commons/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="mt-5">
-        <nav class="navbar navbar-light bg-light justify-content-center">
-                <div class="navbar-brand m-0">© QuestAcademia, All rights reserved.</div>
-        </nav>
-</footer>
--- youtube-curation8888/resources/views/users/followings.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-            @include('users.tabs',['user'=>$user])
-
-            @include('users.users',['users'=>$users])
-
-@endsection
